e3de9af87561336190c53e267f44a3e1d5d23c8a
админ <nebesa566@gmail.com>
2023-01-11 23:34:43 +0300

    vx

--- src/main/java/com/example/calendar/controllers/AdminController.java
+++ src/main/java/com/example/calendar/controllers/AdminController.java
@@ -3,10 +3,9 @@ package com.example.calendar.controllers;
 import com.example.calendar.models.User;
 import com.example.calendar.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,5 +21,20 @@ public class AdminController {
     public List<User> userlist(){
         return userRepository.findAll();
     }
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @DeleteMapping(value = "userListDelete/{id}")
+    public void usersDelete(@PathVariable("id") Long id){
+       userRepository.deleteById(id);
+    }
 
+
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @PostMapping(value = "userListChange/{id}")
+    public void userChange(@PathVariable("id") Long id, @RequestParam String name, @RequestParam String email){
+        User userToUpdate = userRepository.getOne(id);
+        userToUpdate.setUsername(name);
+        userToUpdate.setEmail(email);
+        userRepository.save(userToUpdate);
+    }
 }
